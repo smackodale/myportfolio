3323d276c84a506a11822a32da41a51985e1e64e
Dale Cahill <dalec@dalecnextpathtechcom.onmicrosoft.com>
2026-01-23 10:35:29 +1000

    Update config for deployed version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  clientId: '##{clientid}##',
-  authority: '##{authority}##',
-  redirectUri: '##{redirecturi}##',
-  apiEndpoint: '##{apiendpoint}##',
+  clientId: 'ff95bed1-32b5-41ec-950b-e73f922436a3',
+  authority: 'https://login.microsoftonline.com/5d54e2dd-6af2-444c-ad6b-892ff9699f2a',
+  redirectUri: 'https://salmon-flower-0145bd500.6.azurestaticapps.net/',
+  apiEndpoint: 'https://func-myportfolio-dev-001.azurewebsites.net/api',
 };
